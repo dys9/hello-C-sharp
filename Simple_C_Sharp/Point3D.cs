55dacb48e7cd559d0e508f4efc4711128af28e63
dys9 <dys9@naver.com>
2021-02-04 22:40:18 +0900

    2021.02.04 (3)

---
+++ Simple_C_Sharp/Point3D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_C_Sharp
+{
+    class Point3D
+    {
+        int x, y, z;
+        public int X {
+            get { return x; }
+            set { x = value; } }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }}
+        public int Z
+        {
+            get { return z; }
+            set { z = value; }}
+
+        public Point3D()
+        {
+            this.x = 0; this.y = 0; this.z = 0;
+        }
+
+        public Point3D(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void SortXYZ(Array arr, char c)
+        {
+           //
+        }
+
+        public static Point3D operator +(Point3D a, Point3D b)
+            => new Point3D(a.x + b.x, a.y+b.y, a.z+b.z);
+        public static Point3D operator -(Point3D a, Point3D b)
+            => new Point3D(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+}
