12dd541aa0d06f36b0b7aaa6b43cda7408375d3c
dys9 <dys9@naver.com>
2021-02-04 21:38:40 +0900

    2021.02.04

---
+++ Simple_C_Sharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Simple_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] nums;
+            nums = new int[100];
+
+            for (var i = 0; i < 100; i++)
+            {
+                nums[i] = i - 50;
+            }
+            foreach(var i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
+            Array.Reverse(nums);
+
+            foreach (var i in nums)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
